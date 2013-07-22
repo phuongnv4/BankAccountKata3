a0669cede8f0b7f0ae91cfc81bcdc781f2c47990
phuongnv4 <phuongk48a1t@gmail.com>
2013-07-22 14:06:34 +0700

    complete round 1 kaka 3

--- src/com/qsoft/bank/TransactionDAO.java
+++ src/com/qsoft/bank/TransactionDAO.java
@@ -30,22 +30,34 @@ public class TransactionDAO {
 	}
 
 	public List<TransactionDTO> get(String accountNumber) {
-		Cursor cursor = db.query(DBHelper.TABLE_TRANSACTION, new String[] { DBHelper.KEY_ID, DBHelper.ACCOUNT_NUMBER, DBHelper.AMOUNT,
-				DBHelper.OPEN_TIME_STAMP, DBHelper.DESCRIPTION }, DBHelper.ACCOUNT_NUMBER + "=?", new String[] { accountNumber }, null, null, null);
+		Cursor cursor = db.query(DBHelper.TABLE_TRANSACTION, new String[] {
+				DBHelper.KEY_ID, DBHelper.ACCOUNT_NUMBER, DBHelper.AMOUNT,
+				DBHelper.OPEN_TIME_STAMP, DBHelper.DESCRIPTION },
+				DBHelper.ACCOUNT_NUMBER + "=?", new String[] { accountNumber },
+				null, null, null);
 		return paserCursor(cursor);
 	}
 
-	public List<TransactionDTO> get(String accountNumber, Long startTime, Long stopTime) {
-		String[] columns = new String[] { DBHelper.KEY_ID, DBHelper.ACCOUNT_NUMBER, DBHelper.AMOUNT, DBHelper.OPEN_TIME_STAMP, DBHelper.DESCRIPTION };
-		String whereClause = DBHelper.ACCOUNT_NUMBER + "=? and " + DBHelper.OPEN_TIME_STAMP + ">=? and " + DBHelper.OPEN_TIME_STAMP + "<=?";
-		String[] whereArgs = new String[] { accountNumber, String.valueOf(startTime), String.valueOf(stopTime) };
-		Cursor cursor = db.query(DBHelper.TABLE_TRANSACTION, columns, whereClause, whereArgs, null, null, null);
+	//
+	public List<TransactionDTO> get(String accountNumber, Long startTime,
+			Long stopTime) {
+		String[] columns = new String[] { DBHelper.KEY_ID,
+				DBHelper.ACCOUNT_NUMBER, DBHelper.AMOUNT,
+				DBHelper.OPEN_TIME_STAMP, DBHelper.DESCRIPTION };
+		String whereClause = DBHelper.ACCOUNT_NUMBER + "=? and "
+				+ DBHelper.OPEN_TIME_STAMP + ">=? and "
+				+ DBHelper.OPEN_TIME_STAMP + "<=?";
+		String[] whereArgs = new String[] { accountNumber,
+				String.valueOf(startTime), String.valueOf(stopTime) };
+		Cursor cursor = db.query(DBHelper.TABLE_TRANSACTION, columns,
+				whereClause, whereArgs, null, null, null);
 		return paserCursor(cursor);
 	}
 
 	public List<TransactionDTO> get(String accountNumber, int n) {
-		String countQuery = "SELECT * FROM " + DBHelper.TABLE_TRANSACTION + " WHERE " + DBHelper.ACCOUNT_NUMBER + " = '" + accountNumber + "' LIMIT "
-				+ n;
+		String countQuery = "SELECT * FROM " + DBHelper.TABLE_TRANSACTION
+				+ " WHERE " + DBHelper.ACCOUNT_NUMBER + " = '" + accountNumber
+				+ "' LIMIT " + n;
 		Cursor cursor = db.rawQuery(countQuery, null);
 		return paserCursor(cursor);
 	}
@@ -55,10 +67,14 @@ public class TransactionDAO {
 		if (cursor.moveToFirst()) {
 			do {
 				TransactionDTO transactionDTO = new TransactionDTO();
-				transactionDTO.setAccountNumber(cursor.getString(cursor.getColumnIndex(DBHelper.ACCOUNT_NUMBER)));
-				transactionDTO.setAmount(cursor.getLong(cursor.getColumnIndex(DBHelper.AMOUNT)));
-				transactionDTO.setDescription(cursor.getString(cursor.getColumnIndex(DBHelper.DESCRIPTION)));
-				transactionDTO.setTimestamp(cursor.getLong(cursor.getColumnIndex(DBHelper.OPEN_TIME_STAMP)));
+				transactionDTO.setAccountNumber(cursor.getString(cursor
+						.getColumnIndex(DBHelper.ACCOUNT_NUMBER)));
+				transactionDTO.setAmount(cursor.getLong(cursor
+						.getColumnIndex(DBHelper.AMOUNT)));
+				transactionDTO.setDescription(cursor.getString(cursor
+						.getColumnIndex(DBHelper.DESCRIPTION)));
+				transactionDTO.setTimestamp(cursor.getLong(cursor
+						.getColumnIndex(DBHelper.OPEN_TIME_STAMP)));
 				list.add(transactionDTO);
 			} while (cursor.moveToNext());
 		}
--- test/com/qsoft/test/TransactionDAOTest.java
+++ test/com/qsoft/test/TransactionDAOTest.java
@@ -21,22 +21,26 @@ public class TransactionDAOTest extends AndroidTestCase {
 		transactionDAO = new TransactionDAO(getContext(), null);
 	}
 
+	// 1
 	public void testSaveNewTransaction() {
 		String accountNumber = "0123456789";
 		int amount = 500;
 		String description = "Deposit";
 		Long timestamp = System.currentTimeMillis();
-		TransactionDTO transaction = createNewTransaction(accountNumber, timestamp, amount, description);
+		TransactionDTO transaction = initNewTransaction(accountNumber,
+				timestamp, amount, description);
 		long result = transactionDAO.insert(transaction);
 		assertEquals(1, result);
 	}
 
+	// 2
 	public void testGetTransactionsByAccountNumber() {
 		String accountNumber = "0123456789";
 		int amount = 500;
 		String description = "Deposit";
 		Long timestamp = System.currentTimeMillis();
-		TransactionDTO transaction = createNewTransaction(accountNumber, timestamp, amount, description);
+		TransactionDTO transaction = initNewTransaction(accountNumber,
+				timestamp, amount, description);
 		transactionDAO.insert(transaction);
 		List<TransactionDTO> list = transactionDAO.get(accountNumber);
 		assertTrue(list != null);
@@ -49,14 +53,17 @@ public class TransactionDAOTest extends AndroidTestCase {
 		String description = "Deposit";
 		Long startTime = 1000L;
 		Long stopTime = 2000L;
-		TransactionDTO transaction = createNewTransaction(accountNumber, startTime, amount, description);
+		TransactionDTO transaction = initNewTransaction(accountNumber,
+				startTime, amount, description);
 		transactionDAO.insert(transaction);
 
 		description = "Withdraw";
-		transaction = createNewTransaction(accountNumber, stopTime, -amount, description);
+		transaction = initNewTransaction(accountNumber, stopTime, -amount,
+				description);
 		transactionDAO.insert(transaction);
 
-		List<TransactionDTO> list = transactionDAO.get(accountNumber, startTime, stopTime);
+		List<TransactionDTO> list = transactionDAO.get(accountNumber,
+				startTime, stopTime);
 		assertTrue(list != null);
 		assertEquals(2, list.size());
 	}
@@ -66,16 +73,19 @@ public class TransactionDAOTest extends AndroidTestCase {
 		int amount = 10000;
 		String description = "Deposit";
 		for (int i = 0; i < 10; i++) {
-			TransactionDTO transaction = createNewTransaction(accountNumber, System.currentTimeMillis(), amount + i, description + i);
+			TransactionDTO transaction = initNewTransaction(accountNumber,
+					System.currentTimeMillis(), amount + i, description + i);
 			transactionDAO.insert(transaction);
 		}
 		int numberRecord = 2;
-		List<TransactionDTO> list = transactionDAO.get(accountNumber, numberRecord);
+		List<TransactionDTO> list = transactionDAO.get(accountNumber,
+				numberRecord);
 		assertTrue(list != null);
 		assertEquals(numberRecord, list.size());
 	}
 
-	private TransactionDTO createNewTransaction(String accountNumber, Long timestamp, int amount, String description) {
+	private TransactionDTO initNewTransaction(String accountNumber,
+			Long timestamp, int amount, String description) {
 		TransactionDTO transaction = new TransactionDTO();
 		transaction.setAccountNumber(accountNumber);
 		transaction.setTimestamp(timestamp);
