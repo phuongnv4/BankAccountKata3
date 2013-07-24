2fedd8fa3c803ef5e8f4235438dca5572a9edee8
phuongnv4 <phuongk48a1t@gmail.com>
2013-07-24 14:07:49 +0700

    het vong 2 kata3. hom sau se lam kata4

--- src/com/qsoft/bank/TransactionDTO.java
+++ src/com/qsoft/bank/TransactionDTO.java
@@ -1,41 +1,41 @@
-//package com.qsoft.bank;
-//
-//public class TransactionDTO {
-//	private String accountNumber;
-//	private float amount;
-//	private Long timestamp;
-//	private String description;
-//
-//	public String getAccountNumber() {
-//		return accountNumber;
-//	}
-//
-//	public void setAccountNumber(String accountNumber) {
-//		this.accountNumber = accountNumber;
-//	}
-//
-//	public String getDescription() {
-//		return description;
-//	}
-//
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//
-//	public float getAmount() {
-//		return amount;
-//	}
-//
-//	public void setAmount(float amount) {
-//		this.amount = amount;
-//	}
-//
-//	public Long getTimestamp() {
-//		return timestamp;
-//	}
-//
-//	public void setTimestamp(Long timestamp) {
-//		this.timestamp = timestamp;
-//	}
-//
-//}
+package com.qsoft.bank;
+
+public class TransactionDTO {
+	private String accountNumber;
+	private float amount;
+	private Long timestamp;
+	private String description;
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+}
--- test/com/qsoft/test/TransactionDAOTest.java
+++ test/com/qsoft/test/TransactionDAOTest.java
@@ -1,95 +1,95 @@
-//package com.qsoft.test;
-//
-//import java.util.List;
-//
-//import android.test.AndroidTestCase;
-//
-//import com.qsoft.bank.TransactionDAO;
-//import com.qsoft.bank.TransactionDTO;
-//
-//public class TransactionDAOTest extends AndroidTestCase {
-//
-//	private TransactionDAO transactionDAO;
-//
-//	@Override
-//	public void setUp() throws Exception {
-//		super.setUp();
-//		transactionDAO = new TransactionDAO(getContext(), null);
-//	}
-//
-//	// 1
-//	public void testSaveNewTransaction() {
-//		String accountNumber = "0123456789";
-//		int amount = 500;
-//		String description = "Deposit";
-//		Long timestamp = System.currentTimeMillis();
-//		TransactionDTO transaction = initNewTransaction(accountNumber,
-//				timestamp, amount, description);
-//		long result = transactionDAO.insert(transaction);
-//		assertEquals(1, result);
-//	}
-//
-//	// 2
-//	public void testGetTransactionsByAccountNumber() {
-//		String accountNumber = "0123456789";
-//		int amount = 500;
-//		String description = "Deposit";
-//		Long timestamp = System.currentTimeMillis();
-//		TransactionDTO transaction = initNewTransaction(accountNumber,
-//				timestamp, amount, description);
-//		transactionDAO.insert(transaction);
-//		List<TransactionDTO> list = transactionDAO.get(accountNumber);
-//		assertTrue(list != null);
-//		assertEquals(1, list.size());
-//	}
-//
-//	// 3
-//	public void testGetTransactionsOccurredWithPeriodOfTime() {
-//		String accountNumber = "0123456789";
-//		int amount = 500;
-//		String description = "Deposit";
-//		Long startTime = 1000L;
-//		Long stopTime = 2000L;
-//		TransactionDTO transaction = initNewTransaction(accountNumber,
-//				startTime, amount, description);
-//		transactionDAO.insert(transaction);
-//
-//		description = "Withdraw";
-//		transaction = initNewTransaction(accountNumber, stopTime, -amount,
-//				description);
-//		transactionDAO.insert(transaction);
-//
-//		List<TransactionDTO> list = transactionDAO.get(accountNumber,
-//				startTime, stopTime);
-//		assertTrue(list != null);
-//		assertEquals(2, list.size());
-//	}
-//
-//	// 4
-//	public void testGetTransactionsOccurredWithANumber() {
-//		String accountNumber = "0123456789";
-//		int amount = 10000;
-//		String description = "Deposit";
-//		for (int i = 0; i < 10; i++) {
-//			TransactionDTO transaction = initNewTransaction(accountNumber,
-//					System.currentTimeMillis(), amount + i, description + i);
-//			transactionDAO.insert(transaction);
-//		}
-//		int numberRecord = 2;
-//		List<TransactionDTO> list = transactionDAO.get(accountNumber,
-//				numberRecord);
-//		assertTrue(list != null);
-//		assertEquals(numberRecord, list.size());
-//	}
-//
-//	// 5
-//	private TransactionDTO initNewTransaction(String accountNumber,
-//			Long timestamp, int amount, String description) {
-//		TransactionDTO transaction = new TransactionDTO();
-//		transaction.setAccountNumber(accountNumber);
-//		transaction.setTimestamp(timestamp);
-//		transaction.setAmount(amount);
-//		transaction.setDescription(description);
-//		return transaction;
-//	}
-//}
+package com.qsoft.test;
+
+import java.util.List;
+
+import android.test.AndroidTestCase;
+
+import com.qsoft.bank.TransactionDAO;
+import com.qsoft.bank.TransactionDTO;
+
+public class TransactionDAOTest extends AndroidTestCase {
+
+	private TransactionDAO transactionDAO;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		transactionDAO = new TransactionDAO(getContext(), null);
+	}
+
+	// 1
+	public void testSaveNewTransaction() {
+		String accountNumber = "0123456789";
+		int amount = 500;
+		String description = "Deposit";
+		Long timestamp = System.currentTimeMillis();
+		TransactionDTO transaction = initNewTransaction(accountNumber,
+				timestamp, amount, description);
+		long result = transactionDAO.insert(transaction);
+		assertEquals(1, result);
+	}
+
+	// 2
+	public void testGetTransactionsByAccountNumber() {
+		String accountNumber = "0123456789";
+		int amount = 500;
+		String description = "Deposit";
+		Long timestamp = System.currentTimeMillis();
+		TransactionDTO transaction = initNewTransaction(accountNumber,
+				timestamp, amount, description);
+		transactionDAO.insert(transaction);
+		List<TransactionDTO> list = transactionDAO.get(accountNumber);
+		assertTrue(list != null);
+		assertEquals(1, list.size());
+	}
+
+	// 3
+	public void testGetTransactionsOccurredWithPeriodOfTime() {
+		String accountNumber = "0123456789";
+		int amount = 500;
+		String description = "Deposit";
+		Long startTime = 1000L;
+		Long stopTime = 2000L;
+		TransactionDTO transaction = initNewTransaction(accountNumber,
+				startTime, amount, description);
+		transactionDAO.insert(transaction);
+
+		description = "Withdraw";
+		transaction = initNewTransaction(accountNumber, stopTime, -amount,
+				description);
+		transactionDAO.insert(transaction);
+
+		List<TransactionDTO> list = transactionDAO.get(accountNumber,
+				startTime, stopTime);
+		assertTrue(list != null);
+		assertEquals(2, list.size());
+	}
+
+	// 4
+	public void testGetTransactionsOccurredWithANumber() {
+		String accountNumber = "0123456789";
+		int amount = 10000;
+		String description = "Deposit";
+		for (int i = 0; i < 10; i++) {
+			TransactionDTO transaction = initNewTransaction(accountNumber,
+					System.currentTimeMillis(), amount + i, description + i);
+			transactionDAO.insert(transaction);
+		}
+		int numberRecord = 2;
+		List<TransactionDTO> list = transactionDAO.get(accountNumber,
+				numberRecord);
+		assertTrue(list != null);
+		assertEquals(numberRecord, list.size());
+	}
+
+	// 5
+	private TransactionDTO initNewTransaction(String accountNumber,
+			Long timestamp, int amount, String description) {
+		TransactionDTO transaction = new TransactionDTO();
+		transaction.setAccountNumber(accountNumber);
+		transaction.setTimestamp(timestamp);
+		transaction.setAmount(amount);
+		transaction.setDescription(description);
+		return transaction;
+	}
+}
